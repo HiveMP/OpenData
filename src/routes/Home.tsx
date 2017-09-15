856d92b6848bbd55a077ef4d09f50ef10c5b0ba9
June Rhodes <jrhodes@redpointgames.com.au>
2017-09-15 22:09:56 +1000

    Update home page content to add links to search

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AppState } from '../state/AppState';
 import LoginButton from '../components/LoginButton';
+import { Link } from 'react-router-dom';
 
 interface Props {
   appState: AppState;
@@ -12,20 +13,61 @@ export default class Home extends React.Component<Props, {}> {
   }
 
   render() {
-    
-    return (
-      <div className="starter-template">
-        <h1>HiveMP Open Data</h1>
-        <p className="lead">
-          View trends and analyse public gaming data.
-        </p>
+    let content: JSX.Element;
+    if (this.props.appState.response !== undefined &&
+        this.props.appState.response.error === undefined) {
+      content = (
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <div className="card">
+              <div
+                className="card-img-top card-icon"
+                style={{backgroundColor: '#000000', color: 'white'}}
+              >
+                <i className="fa fa-steam" />
+              </div>
+              <div className="card-body">
+                <h4 className="card-title">Steam Curators</h4>
+                <p className="card-text">View trends of Steam Curators over time.</p>
+                <Link to="/steam/curators" className="btn btn-primary">Search now</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6">
+            <div className="card">
+              <div
+                className="card-img-top card-icon"
+                style={{backgroundColor: 'white', color: '#ff4500'}}
+              >
+                <i className="fa fa-reddit" />
+              </div>
+              <div className="card-body">
+                <h4 className="card-title">Reddit</h4>
+                <p className="card-text">Search for mentions of games on Reddit in real-time.</p>
+                <Link to="/reddit" className="btn btn-primary">Search now</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      content = (
         <p className="lead">
           <LoginButton
             appState={this.props.appState}
             className="btn btn-success btn-primary my-2 my-sm-0"
             text="Login with Google to get started"
           />
-          </p>
+        </p>
+      );
+    }
+    return (
+      <div className="starter-template">
+        <h1>HiveMP Open Data</h1>
+        <p className="lead">
+          View trends and analyse public gaming data.
+        </p>
+        {content}
       </div>
     );
   }
